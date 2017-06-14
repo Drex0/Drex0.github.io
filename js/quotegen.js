8bfde112bbf0ddee001f87037382106e438ed2cc
Patrick <pzachman@gmail.com>
2017-06-14 16:08:33 -0600

    First Commit
    Launch of Portfolio

---
+++ js/quotegen.js
@@ -0,0 +1,14 @@
+var quotes = [
+	"Saving the world from bad audio one venue at a time. -PZ",
+	"As it turns out, cubicle life isn\'t just like Workaholics. - PZ",
+	"It is only a matter of time until I can programmatically change the rotation of the Earth. -PZ",
+	"I couldn't have done it without NOS and Slimjims. -PZ",
+	"Consultant for common sense. This is how I will make my millions. -PZ",
+	"It\'s been proven over and over, happy employees work harder. You should hire me strictly for my sense of humor. -PZ"
+]
+
+function newQuote() {
+	var randomNumber = Math.floor(Math.random() * (quotes.length));
+	document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+	
+}
